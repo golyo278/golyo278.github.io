498ca22d0fbcf088bad0b36b44bb88afd5f60ccb
madison <madison.bozinoff@gmail.com>
2023-05-11 00:07:41 -0400

    fix index.html, fix info.hmtl, fix videos across site

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+var images = [
+	{
+		file_type: "jpg", 
+		headline: "Here’s What Paying Off Your Student Loans Can Do To Your Credit Score", 
+		year: "2023", 
+		work_type: "Editorial",  
+		client: "BuzzFeed News", 
+		illustrator_credit: ""
+	}, {
+		file_type: "jpg", 
+		headline: "Here’s What Paying Off Your Student Loans Can Do To Your Credit Score", 
+		year: "2023", 
+		work_type: "Editorial",  
+		client: "BuzzFeed News", 
+		illustrator_credit: ""
+	}, {
+		file_type: "jpg", 
+		headline: "Secure The Bag", 
+		year: "2023", 
+		work_type: "Editorial, Package Identity",  
+		client: "BuzzFeed News", 
+		illustrator_credit: ""
+	}, {
+		file_type: "jpg", 
+		headline: "FDA Wants Doctors To Better Inform Patients Of Possible LASIK Complications, Including “Psychological Harm”", 
+		year: "2022", 
+		work_type: "Editorial",  
+		client: "BuzzFeed News", 
+		illustrator_credit: ""
+	}, {
+		file_type: "jpg", 
+		headline: "I Read 365 Books In A Single Year. Here’s What I Actually Liked", 
+		year: "2023", 
+		work_type: "Editorial",  
+		client: "BuzzFeed News", 
+		illustrator_credit: ""
+	} 
+];
+
+for (var i = images.length - 1; i >= 0; i--) {
+	console.log(images[i].headline);
+};
